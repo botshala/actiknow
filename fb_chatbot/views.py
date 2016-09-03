a20bd5cc3eec866cd59dd12e50ef2cea4d73b18a
Saurav Tomar <stomatrix@gmail.com>
2016-09-03 18:06:29 +0530

    another commit

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -33,8 +33,8 @@ def post_facebook_message(fbid, recevied_message):
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
     user_details_params = {'access_token':PAGE_ACCESS_TOKEN}
     user_details = requests.get(user_details_url, user_details_params).json()
-    print user_details_url
-    print user_details
+    #print user_details_url
+    #print user_details
     #customer_name = user_details['name']
     #customer_name = customer_name.split(" ")[0]
     customer_name = ''
