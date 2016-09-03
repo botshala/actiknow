572281e6529646be62656385c9f22ca3dbe1e622
Saurav Tomar <stomatrix@gmail.com>
2016-09-03 16:32:51 +0530

    another commit

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -23,7 +23,8 @@ def post_facebook_message(fbid, recevied_message):
     user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
     user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
     user_details = requests.get(user_details_url, user_details_params).json()
-    customer_name = user_details['first_name']
+    customer_name = user_details['name']
+    customer_name = customer_name.split(" ")[0]
 
     if recevied_message.startswith('/machineid'):
         machineid = recevied_message.replace('/machineid','')
