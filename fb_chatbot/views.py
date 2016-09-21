09de5411ce8b9f3351da5d0a698ae8ee694d824f
aakash-cr7 <aakash.tyg93@gmail.com>
2016-09-21 15:29:51 +0530

    Fix minor bug.

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -11,7 +11,7 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from .models import Ticket
+from .models import Ticket, Customer
 import datetime
 
 import pytz
